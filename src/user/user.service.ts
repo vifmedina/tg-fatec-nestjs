27524993ea0164f9256899710fe76380bf53130a
Vinicius Medina <vinicius.fernandes.medina@gmail.com>
2026-08-29 23:57:45 -0300

    feat: user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,53 @@
 import { Injectable } from '@nestjs/common';
+import * as mysql from 'mysql2/promise'
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  private pool: mysql.Pool;
+
+  constructor() {
+    this.pool = mysql.createPool({
+      host: 'localhost',
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async create(createUserDto: CreateUserDto) {
+    const { name, age, status } = createUserDto;
+    const [result] = await this.pool.execute(
+      'INSERT INTO users (name, age, status) VALUES (?, ?, ?)',
+      [name, age, status],
+    );
+    return { id: (result as any).insertId, ...createUserDto };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findAll() {
+    const [rows] = await this.pool.execute('SELECT * FROM users');
+    return rows;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOne(id: number) {
+    const [rows] = await this.pool.execute('SELECT * FROM users WHERE id = ?', [id]);
+    return (rows as any)[0];
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const { name, age, status } = updateUserDto;
+    await this.pool.execute(
+      'UPDATE users SET name = ?, age = ?, status = ? WHERE id = ?',
+      [name, age, status, id] as any[],
+    );
+    return { id, ...updateUserDto };
+  }
+
+  async remove(id: number) {
+    await this.pool.execute('DELETE FROM users WHERE id = ?', [id]);
+    return { message: `User #${id} deleted successfuly`};
   }
 }
